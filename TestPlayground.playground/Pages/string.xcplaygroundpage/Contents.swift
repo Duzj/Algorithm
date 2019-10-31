a5473346ac77f8b6e3b010ad66dc62add785cef0
duzj <duzhijian@syswin.com>
2019-10-31 19:33:23 +0800

    add code

--- TestPlayground.playground/Pages/string.xcplaygroundpage/Contents.swift
+++ TestPlayground.playground/Pages/string.xcplaygroundpage/Contents.swift
@@ -5,6 +5,59 @@ import Foundation
 var str = "字符串问题"
 //MARK: - 字符串问题
 
+str = "3. 无重复字符的最长子串"
+print(str)
+/*
+ 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ 示例 1:
+ 输入: "abcabcbb"
+ 输出: 3
+ 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ 示例 2:
+ 输入: "bbbbb"
+ 输出: 1
+ 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ 示例 3:
+
+ 输入: "pwwkew"
+ 输出: 3
+ 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        
+        for i in 0 ..< s.lengthOfBytes(using: .utf8){
+            
+            for j in 0 ..< s.lengthOfBytes(using: .utf8){
+                let startIndex = s.index(s.startIndex, offsetBy: i)
+                let endIndex = s.index(s.startIndex, offsetBy: j)
+                let str = s[startIndex ... endIndex]
+                
+            }
+            
+        }
+        
+        return 0
+    }
+    
+    func allUnique(_ str : String) -> Bool {
+        var tempSet = Set<Character>()
+        for char in str {
+            if tempSet.contains(char){
+                return false
+            }else{
+                tempSet.insert(char)
+            }
+        }
+        return true
+    }
+}
 
 
 
@@ -23,7 +76,7 @@ var str = "字符串问题"
  链接：https://leetcode-cn.com/problems/find-common-characters
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-class Solusion{
+class Solusion1{
     func commonChars(_ A : [String]) -> [String] {
         if A.isEmpty{
             return [String]()
